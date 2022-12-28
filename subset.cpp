7a665120163b0e985416b0f4e43b09c1fe8106fa
Gayatri Bhosale <gayatribhosale2610@outlook.com>
2022-12-28 14:23:10 +0530

    Create subset.cpp

---
+++ subset.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    void solve(vector<int> nums, vector<int> res, int indx, vector<vector<int>>& ans){
+        if(indx >= nums.size()){
+            ans.push_back(res);
+            return;
+        }
+        // exclude
+        solve(nums, res, indx + 1, ans);
+        
+        // include
+        int ele = nums[indx];
+        res.push_back(ele);
+        solve(nums, res, indx+1, ans);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> res;
+        int indx = 0;
+        solve(nums, res, indx, ans);
+        return ans;
+    }
+};
